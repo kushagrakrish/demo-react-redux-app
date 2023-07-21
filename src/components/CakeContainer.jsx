534467bb41bea5e285b9381ab711b28ef7ca8316
@kushagra24 <krishna619kk@gmail.com>
2023-07-21 10:07:42 +0530

    Created a new react app with redux and react-redux

---
+++ src/components/CakeContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CakeContainer = () => {
+  return (
+    <>
+      <h2>Number of cakes</h2>
+      <button>Buy cakes</button>
+    </>
+  );
+};
+
+export default CakeContainer;
